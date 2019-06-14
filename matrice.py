357292e4746a17654a15a8332ad9d9e46e4f0baa
munen56 <florian.quiec@laposte.net>
2019-06-14 21:20:41 +0200

    bug deconcertant ligne de O -rappel d'__init__ ?????????????

--- matrice.py
+++ matrice.py
@@ -1,11 +1,12 @@
 # coding=utf-8
 import unittest
+import random
 
 
 class Matrix(object):
 
 
-    def __init__(self, line=1, column=3, fill_motif="O", border=True):
+    def __init__(self, line=1, column=3, fill_motif=" ", border=True):
 
 
         self.line_num = line
@@ -27,6 +28,27 @@ class Matrix(object):
             self.matrix[self.line_num - 1] = list("#" * self.column_num)
 
 
+    def place_patern(self, pattern):
+        """positionne un pattern en haut, au centre de la matrice"""
+        X = 0 #premiere ligne (en haut) de la matrice
+        Y = int(self.line_num / 2) #milieu d'une ligne(si elle est paire ^^).
+
+        self.matrix[3][0] = "o"
+
+
+        matrix_display = ""
+        for line in range(len(self.matrix)): # on concatene toute les ligne en y intercalant  un retour chariot
+            matrix_display += "".join(self.matrix[line]) + "\n"
+        print(matrix_display)
+        """
+        for x, value in pattern.items():
+            for y, _ in enumerate(value):
+
+                self.matrix[X+x][Y+y] =  "O"
+        """
+
+
+"""
     def __repr__(self):
 
 
@@ -36,9 +58,45 @@ class Matrix(object):
 
         return matrix_display
 
+"""
 
 
 
 
-play_ground = Matrix(40, 80)
-print(play_ground)
+#-----------------------------------------------------------------------------------------
+
+class pattern(object):
+
+
+    def __init__(self, choice=1):
+
+        self.choice = choice
+        self.coord = {} # dictionnaire qui contient la description des pattern  0 = 2,3,4 ou de la forme x = y+2, y+3, y+4
+
+        if choice == "random":
+            random.choice(range(8))
+
+        if choice == 1: #truc décalé
+            self.coord = {0: [1, 2], 1: [0,1] }
+
+
+
+        #elif choice == 2:
+        #elif choice == 3:
+        #elif choice == 4:
+        #elif choice == 5:
+        #elif choice == 6:
+        #elif choice == 7:
+
+    def __repr__(self):
+        return self.coord
+#-----------------------------------------------------------------------------------------------------------
+
+
+play_ground = Matrix(5, 15)
+print(play_ground.matrix)
+
+
+play_ground.place_patern(pattern(1).coord)
+print(play_ground.matrix)
+print(play_ground.matrix_col)
