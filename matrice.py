c1e32322fe0ce2afdc77038afdab114de90c5771
munen56 <florian.quiec@laposte.net>
2019-06-27 18:36:32 +0200

    premier ménage

--- matrice.py
+++ matrice.py
@@ -8,7 +8,11 @@ import math as mt
 
 
 class Matrix(object):
-    #scinder en deux classe pattern_operation et matrix
+    """
+        Cette classe permet la cretion d'un tableau a la taille de la grille de jeu.elle fournit des methodes pour
+         l'initialisation d'un motif sur cette grille ainsi que pour sont deplacement lateral, verticale , sa rotation.
+         todo scinder en deux classe pattern_operation et matrix
+    """
 
     def __init__(self, line=1, column=3, fill_motif=" ", border=True):
         self.line_num = line
@@ -31,45 +35,43 @@ class Matrix(object):
                 self.matrix[indice][self.column_num - 1] = "#"
             self.matrix[self.line_num - 1] = list("#" * self.column_num)
 
-    def coordonné_vers_matrice(self, dico_de_coordonné, symbole):
+    def _coordinate_to_matrix(self, coordinate_dictionary, symbole):
 
-        for x, y_group in dico_de_coordonné.items():
+        for x, y_group in coordinate_dictionary.items():
             for _, y in enumerate(y_group):
                 self.matrix[x][y] = symbole
 
-    def draw_vertival(self, y ):
+    def draw_vertival(self, y):
         for line in self.matrix:
             line[y] = "#"
 
     def draw_message(self, mess="default message", x=10, y=6):
 
         for indice, _ in enumerate(self.matrix[x]):
-            if indice >= y and indice < len(mess) + y:
-                self.matrix[x][indice] = mess[indice-y]
-
-
+            if y <= indice < len(mess) + y:
+                self.matrix[x][indice] = mess[indice - y]
 
     def place_mobile_patern(self, pattern, y):
         """positione un pattern en haut, au centre de la matrice"""
 
         x_origin = 0  # premiere ligne (en haut) de la matrice
-        #y_middle = self.column_num // 2  # milieu d'une ligne(si elle est paire ^^).
+        # y_middle = self.column_num // 2  # milieu d'une ligne(si elle est paire ^^).
         y_middle = y
         for x in pattern.keys():  # on actualise les coordonné des items pour les placer au milieux
             self.last_coord[x_origin + x] = [y_pattern + y_middle for y_pattern in pattern[x]]
 
-        Matrix.coordonné_vers_matrice(self, self.last_coord, "0")
+        Matrix._coordinate_to_matrix(self, self.last_coord, "0")
 
     def place_patern(self, pattern, motif, x, y):
 
-        coordone={}
+        coordone = {}
         x_origin = x  # premiere ligne (en haut) de la matrice
 
         y_middle = y
         for x in pattern.keys():  # on actualise les coordonné des items pour les placer au milieux
             coordone[x_origin + x] = [y_pattern + y_middle for y_pattern in pattern[x]]
 
-        Matrix.coordonné_vers_matrice(self, coordone, motif)
+        Matrix._coordinate_to_matrix(self, coordone, motif)
 
     def _bottom_collision(self):
         collision = False
@@ -81,9 +83,6 @@ class Matrix(object):
 
         return collision
 
-    #def _complete_line(self):
-
-
     def _side_collision(self, direction):
         collision = False
 
@@ -94,48 +93,50 @@ class Matrix(object):
                 if direction == "right" and self.matrix[x][y + 1] == "O" or self.matrix[x][y + 1] == "#":
                     collision = True  # coté droit
         return collision
+
     # rotation group ---------------------------------------------------------------------------------------------------
+
     def _rotation_center(self):
+
         """return les coordonnés du centre de rotation du motif actuelle"""
 
-        center_x = max(self.last_coord.keys()) -1
+        center_x = max(self.last_coord.keys()) - 1
         for y in self.last_coord.values():
-            center_y = (min(y)+((max(y) - min(y)) // 2))
+            center_y = (min(y) + ((max(y) - min(y)) // 2))
 
         return center_x, center_y
 
-    def rotate(self,car, theta=90):
+    def rotate(self, theta=90):
 
+        rotated_coordinate = {}
+        list_coo = []
+        theta_radians = mt.radians(theta)
 
-            rotated_coordinate = {}
-            mini=0
-            list_coo=[]
-            theta_radians = mt.radians(theta)
-            x_prims = []
-            y_tourné = []
-            center_x, center_y = Matrix._rotation_center(self)
+        center_x, center_y = Matrix._rotation_center(self)
 
+        for x, y_group in self.last_coord.items():
+            for y in y_group:
+                rotated_x = round(
+                    ((x - center_x) * mt.cos(theta_radians)) - ((y - center_y) * mt.sin(theta_radians))) + center_x
+                rotated_y = round(
+                    ((x - center_x) * mt.sin(theta_radians)) + ((y - center_y) * mt.cos(theta_radians))) + center_y
+                list_coo.append((rotated_x, rotated_y))
 
-            for x, y_group in self.last_coord.items():
-                for y in y_group:
-                    x_tourné = round(((x- center_x) * mt.cos(theta_radians)) - ((y- center_y) * mt.sin(theta_radians))) + center_x
-                    y_tourné =  round(((x- center_x) * mt.sin(theta_radians)) + ((y- center_y) * mt.cos(theta_radians))) + center_y
-                    list_coo.append((x_tourné, y_tourné))
+        for couple in list_coo:
+            rotated_coordinate[couple[0]] = []
 
-            for couple in list_coo:
-                rotated_coordinate[couple[0]]=[]
+        for couple in list_coo:
+            rotated_coordinate[couple[0]].append(couple[1])
 
-            for couple in list_coo:
-                rotated_coordinate[couple[0]].append(couple[1])
+        if min(rotated_coordinate.keys()) > 0:  # on empeche x negatif = pas de rotation sur les premiere lignes
+            Matrix._coordinate_to_matrix(self, self.last_coord, " ")
+            self.last_coord = rotated_coordinate
 
-            if min(rotated_coordinate.keys()) > 0: #on empeche x negatif = pas de rotation sur les premiere lignes
-                Matrix.coordonné_vers_matrice(self, self.last_coord, " ")
-                self.last_coord = rotated_coordinate
-
-    # ------------------------------------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------------------------------
     def translate(self, direction):
 
-        Matrix.coordonné_vers_matrice(self, self.last_coord, " ")  # risque d'effacement du pattern lors de l'appel a translate si on commente la ligne les traces laissé ne constituent pas des points d'arrets ???
+        Matrix._coordinate_to_matrix(self, self.last_coord, " ")  # risque d'effacement du pattern lors de l'appel a
+        # translate si on commente la ligne les traces laissé ne constituent pas des points d'arrets ???
 
         if direction == "left" and not Matrix._side_collision(self, "left"):
             Matrix._deplacement(self, "left")
@@ -156,21 +157,21 @@ class Matrix(object):
 
     def down(self):
 
-        nouveau_coordonné = {}
+        new_coordinate = {}
 
         if Matrix._bottom_collision(self):
-            Matrix.coordonné_vers_matrice(self, self.last_coord, "O")  # ligne utile ????
+            Matrix._coordinate_to_matrix(self, self.last_coord, "O")  # ligne utile ????
             self.last_coord = {}
             return False
 
         else:
             for x, y in self.last_coord.items():
-                nouveau_coordonné[x + 1] = y
+                new_coordinate[x + 1] = y
 
-            Matrix.coordonné_vers_matrice(self, self.last_coord, " ")
-            Matrix.coordonné_vers_matrice(self, nouveau_coordonné, "0")
+            Matrix._coordinate_to_matrix(self, self.last_coord, " ")
+            Matrix._coordinate_to_matrix(self, new_coordinate, "0")
 
-            self.last_coord = nouveau_coordonné
+            self.last_coord = new_coordinate
 
             return True
 
@@ -185,54 +186,58 @@ class Matrix(object):
 
 # -----------------------------------------------------------------------------------------
 
-class pattern(object):
+class Pattern(object):
 
     def __init__(self, choice=1):
 
-        self.coordonné_pour_affichage = {}  # dictionnaire qui contient les coordonés des pattern  0 = 2,3,4 de la forme x = y+2, y+3, y+4
+        self.coordinate_for_display = {}  # dictionnaire qui contient les coordonés des pattern  0 = 2,3,4 de la forme
+        # x = y+2, y+3, y+4
 
         if choice == "random":
             choice = random.randrange(1, 7)
 
         if choice == 1:  # truc décalé
-            self.coordonné_pour_affichage = {0: [1, 2], 1: [0, 1]}
+            self.coordinate_for_display = {0: [1, 2], 1: [0, 1]}
 
         elif choice == 2:  # T a l'envers
-            self.coordonné_pour_affichage = {0: [1], 1: [0, 1, 2]}
+            self.coordinate_for_display = {0: [1], 1: [0, 1, 2]}
 
         elif choice == 3:  # carre
-            self.coordonné_pour_affichage = {0: [0, 1], 1: [0, 1]}
+            self.coordinate_for_display = {0: [0, 1], 1: [0, 1]}
         elif choice == 4:  # l
-            self.coordonné_pour_affichage = {0: [2], 1: [0, 1, 2]}
+            self.coordinate_for_display = {0: [2], 1: [0, 1, 2]}
 
-        elif choice == 5: # L'autre l
-            self.coordonné_pour_affichage = {0: [0], 1: [0, 1, 2]}
+        elif choice == 5:  # L'autre l
+            self.coordinate_for_display = {0: [0], 1: [0, 1, 2]}
 
-        elif choice == 6: # baton
-            self.coordonné_pour_affichage = {0: [0, 1, 2, 3]}
+        elif choice == 6:  # baton
+            self.coordinate_for_display = {0: [0, 1, 2, 3]}
 
         else:
             print("error")
 
     def __repr__(self):
-        return self.coordonné_pour_affichage
+        return self.coordinate_for_display
 
 
 # -----------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
+    #  bugs le carré ne descensd pas si on le tourne en continue,
+    #  les piece peuvent s'attacher au mur lors d'une rotation, les piece se magnetisent au mur
     line = 20
     column = 40
     level = 0.5
     play_ground = Matrix(line, column)
     score = 0
+
+
     def display_score(score):
 
         play_ground.draw_message("************", 5, 27)
         play_ground.draw_message(" YOUR SCORE:", 6, 27)
-        play_ground.draw_message("     %s" %(score), 7, 27)
+        play_ground.draw_message("     %s" % score, 7, 27)
         play_ground.draw_message("************", 8, 27)
-
 
 
     def game_over():
@@ -251,15 +256,15 @@ if __name__ == "__main__":
 
         return continu_play_loop
 
+
     def line_completed(score=0):
 
         for indice, line in enumerate(play_ground.matrix):
             if line[1:score_line_y] == ["O"] * (score_line_y - 1):
-                del(play_ground.matrix[indice])
-                #play_ground.matrix.insert(0, play_ground.matrix[0])
+                del (play_ground.matrix[indice])
+                # play_ground.matrix.insert(0, play_ground.matrix[0])
                 score += 100
         return score
-
 
 
     def keyboard_input():
@@ -267,27 +272,23 @@ if __name__ == "__main__":
         check = msvcrt.kbhit()
         if check:
             car = msvcrt.getch()
-        return  car
+        return car
 
-    score_line_y = 2* column//3
+
+    score_line_y = 2 * column // 3
     play_ground.draw_vertival(score_line_y)
-
 
     display_score(score)
 
-
-
-
     play_loop = True
-    piece_suivante = pattern("random")
+    piece_suivante = Pattern("random")
     while play_loop:
-        play_ground.place_patern(piece_suivante.coordonné_pour_affichage, " ", 2, 32)
+        play_ground.place_patern(piece_suivante.coordinate_for_display, " ", 2, 32)
         piece_en_cours = piece_suivante
-        piece_suivante = pattern("random")
+        piece_suivante = Pattern("random")
 
-        play_ground.place_patern(piece_suivante.coordonné_pour_affichage,"0" , 2, 32)
-        play_ground.place_mobile_patern(piece_en_cours.coordonné_pour_affichage, score_line_y//2)
-
+        play_ground.place_patern(piece_suivante.coordinate_for_display, "0", 2, 32)
+        play_ground.place_mobile_patern(piece_en_cours.coordinate_for_display, score_line_y // 2)
 
         continu = True
         while continu:
@@ -301,7 +302,7 @@ if __name__ == "__main__":
                 command_carachters = keyboard_input()
                 if command_carachters != 0:
                     if command_carachters == b"z":
-                        play_ground.rotate(command_carachters)
+                        play_ground.rotate()
                         break
 
                     elif command_carachters == b"s":
@@ -319,6 +320,3 @@ if __name__ == "__main__":
         play_loop = game_over()
         display_score(score)
         score += line_completed()
-
-
-
